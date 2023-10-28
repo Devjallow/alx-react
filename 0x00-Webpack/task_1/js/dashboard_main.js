fbd58fd72a34d835eb712c7db1112aa5cefa8b5f
Devjallow <bjallow000@outlook.com>
2023-10-28 16:02:42 +0000

    intial commit

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,26 +1,21 @@
-import $ from 'jquery';
-import _ from 'lodash';
+const $ = require( "jquery" );
+// Load the full build
+const _ = require("lodash");
 
-// Create and append elements
-$(document).ready(function () {
-  // Create and append paragraphs and button
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="startButton">Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p');
+let count = 0;
 
-  // Initialize the count
-  let count = 0;
+function updateCounter() {
+  count++;
+  $( "#count" ).text(`${count} clicks on the button` );
+}
 
-  // Update the count and display it when the button is clicked
-  function updateCounter() {
-    count++;
-    $('#count').text(count + ' clicks on the button');
-  }
+const $button = $("<button>Click here to get started</button>").on(
+  "click",
+  _.debounce( updateCounter, 500, { leading: true, trailing: false })
+);
 
-  // Bind the debounce function to the click event on the button
-  const debouncedUpdateCounter = _.debounce(updateCounter, 500);
-  $('#startButton').on('click', debouncedUpdateCounter); // Adjust the debounce delay as needed
-});
-
+$('body').append("<p>Holberton Dashboard</p>");
+$('body').append("<p>Dashboard data for the students</p>");
+$('body').append($button);
+$('body').append("<p id='count'></p>");
+$('body').append("<p>Copyright - Holberton School</p>");
